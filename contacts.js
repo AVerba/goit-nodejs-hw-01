e41328023bc7c1fa11690f361fa19732cafb6263
Oleksii Verbytskyi <alverba88@gmail.com>
2022-07-08 16:34:10 +0300

    fix get function mistakes

--- contacts.js
+++ contacts.js
@@ -3,7 +3,7 @@ const path = require("path");
 const ObjectID = require("bson-objectid");
 
 const contactsPath = path.join(__dirname, "./db/contacts.json");
-const updateContactsList = async (contacts)=> {
+const updateContactsList = async (contacts) => {
     await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
 }
 
@@ -16,7 +16,7 @@ const listContacts = async () => {
 const getContactById = async (contactId) => {
     const contacts = await listContacts();
     const result = contacts.find(item => item.id === contactId);
-    if(!result){
+    if (!result) {
         return null;
     }
     return result;
@@ -26,7 +26,7 @@ const getContactById = async (contactId) => {
 const removeContact = async (contactId) => {
     const contacts = await listContacts();
     const idx = contacts.findIndex(item => item.id === contactId);
-    if(idx === -1){
+    if (idx === -1) {
         return null;
     }
     const [removeContact] = contacts.splice(idx, 1);
@@ -38,10 +38,10 @@ const removeContact = async (contactId) => {
 const addContact = async (name, email, phone) => {
     const contacts = await listContacts();
     const newContact = {
+        id: ObjectID(),
         name,
         email,
         phone,
-        id: ObjectID()
     };
     contacts.push(newContact);
     updateContactsList(contacts);
