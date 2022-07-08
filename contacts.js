40a154bbe85f60fda449f9a9bcf4710d0fa82254
Oleksii Verbytskyi <alverba88@gmail.com>
2022-07-08 14:06:25 +0300

    added hw

--- contacts.js
+++ contacts.js
@@ -2,21 +2,50 @@ const fs = require("fs/promises");
 const path = require("path");
 const ObjectID = require("bson-objectid");
 
-const booksPath = path.join(__dirname, "contacts.json");
+const contactsPath = path.join(__dirname, "contacts.json");
+const updateContactsList = async (contacts)=> {
+    await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+}
 
 const listContacts = async () => {
+    const data = await fs.readFile(contactsPath);
+    return JSON.parse(data);
 
 }
 
 const getContactById = async (contactId) => {
+    const contacts = await listContacts();
+    const result = contacts.find(item => item.id === id);
+    if(!result){
+        return null;
+    }
+    return result;
 
 }
 
 const removeContact = async (contactId) => {
+    const contacts = await listContacts();
+    const idx = contacts.findIndex(item => item.id === contactId);
+    if(idx === -1){
+        return null;
+    }
+    const [removeContact] = contacts.splice(idx, 1);
+    updateContactsList(contacts);
+    return removeContact;
 
 }
 
 const addContact = async (name, email, phone) => {
+    const contacts = await listContacts();
+    const newContact = {
+        name,
+        email,
+        phone,
+        id: ObjectID()
+    };
+    contacts.push(newContact);
+    updateContactsList(contacts);
+    return newContact;
 
 }
 
--- index.js
+++ index.js
@@ -1,22 +1,27 @@
-const{program} = require("commander");
+const {program} = require("commander");
 
+const contacts = require("./contacts");
 
-function invokeAction({ action, id, name, email, phone }) {
+const invokeAction = async ({action, id, name, email, phone}) => {
     switch (action) {
         case 'list':
-
+            const result = await contacts.listContacts();
+            console.log(result);
             break;
 
         case 'get':
-
+            const contact = await contacts.getContactById(id);
+            console.log(contact);
             break;
 
         case 'add':
-
+            const newContact = await contacts.addContact(name, email, phone)
+            console.log(newContact);
             break;
 
         case 'remove':
-
+            const removeContact = await contacts.removeContact(id)
+            console.log(removeContact);
             break;
 
         default:
